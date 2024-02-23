334b50d54e8b18aada7c91b588fd2ab2f75137b0
LeonieWhitehead <leoniewhitehead@hotmail.co.uk>
2024-02-23 18:50:35 +0000

    added team member array and function for the inquirer prompts

--- starter/index.js
+++ starter/index.js
@@ -13,3 +13,32 @@ const render = require("./src/page-template.js");
 
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
 
+// Create an empty array to store team members
+const team = [];
+
+//Function to add a new member to the team
+const addEmployee = (employeeType) => {
+  switch (employeeType) {
+    case 'Engineer':
+      return inquirer.prompt([
+        {
+          type: 'input',
+          name: 'name',
+          message: "What's the engineer's name?",
+        },
+        {
+          type: 'input',
+          name: 'id',
+          message: "What's the engineer's ID?",
+        },
+        {
+          type: 'input',
+          name: 'email',
+          message: "What's the engineer's email?",
+        },
+        {
+          type: 'input',
+          name: 'github',
+          message: "What's the engineer's GitHub username?",
+        },
+      ])
